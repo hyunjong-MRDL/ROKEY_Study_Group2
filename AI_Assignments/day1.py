159365ca7b1e3a5b3484aece9781ad8a945a89cd
hyunjong-MRDL <hyunjongkim0524@gmail.com>
2025-08-14 20:15:57 +0900

    day1

---
+++ AI_Assignments/day1.py
@@ -0,0 +1,10 @@
+flower_price = {
+    'rose': 10000,
+    'iris': 3000,
+    'stargauge': 12000,
+    'lily': 9000,
+    'daffodil': 3000
+}
+
+for key in flower_price:
+    print(flower_price[key])
