dba080ad108d27ac86673d2c7cbcb60c90dda772
Yangsw197 <yangsw197@gmail.com>
2025-09-06 20:13:54 +0900

    20250906study_sw

---
+++ Algorithm_6_seunwon.py
@@ -0,0 +1,25 @@
+#백준 13398번 전체 코드
+# import sys
+# sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+L = [0]*N
+L[0] = A[0]
+
+R = [0]*N
+R[N-1] = A[N-1]
+
+for i in range(1,N):
+  L[i] = max(A[i], L[i-1]+A[i])
+
+for i in range(N-2,-1,-1):
+  R[i] = max(A[i], R[i+1]+A[i])
+
+result = max(L)
+
+for i in range(1,N-1):
+  result = max(result, L[i-1]+R[i+1])
+
+print(result)
--- test_sw.py
+++ test_sw.py
@@ -1,1 +1,36 @@
-print('스터디 조원 양승원 입니다')
+#다익스트라 백준 1753
+import heapq
+import sys
+input = sys.stdin.readline
+
+# 1. 입력 및 초기화
+V, E = map(int, input().split())
+K = int(input())
+graph = [[] for _ in range(V + 1)]
+INF = sys.maxsize
+distance = [INF] * (V + 1)
+
+# 2. 간선 입력
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
+
+# 3. 다익스트라
+def dijkstra(start):
+    distance[start] = 0
+    heap = [(0, start)]
+
+    while heap:
+        dist, now = heapq.heappop(heap)
+        if dist > distance[now]:
+            continue
+        for neighbor, weight in graph[now]:
+            cost = dist + weight
+            if cost < distance[neighbor]:
+                distance[neighbor] = cost
+                heapq.heappush(heap, (cost, neighbor))
+
+# 4. 실행 및 출력
+dijkstra(K)
+for i in range(1, V + 1):
+    print("INF" if distance[i] == INF else distance[i])
